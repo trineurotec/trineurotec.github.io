e1ef4c75d928b8f78bda2730e52ffd29b8e18d45
최상진 <hhclover@kaist.ac.kr>
2017-09-06 22:41:52 +0900

    asdf

---
+++ _posts/2017-9-6-Trineurotec.markdown
@@ -0,0 +1,9 @@
+---
+layout: post
+title:  Reinforcing effects of methamphetamine in an animal model of Attention-Deficit/Hyperactivity Disorder-the Spontaneously Hypertensive Rat
+date:   2017-09-06 04:04:36 +0900
+categories: jekyll update
+author: Trineurotec
+---
+
+Substrains of the Spontaneously Hypertensive rat (SHR), a putative animal model of Attention-Deficit/Hyperactivity Disorder (ADHD), have demonstrated increased sensitivity to many drugs of abuse, including psychostimulants. Therefore, it was suggested that studies in SHR may help elucidate ADHD and comorbidity with substance use disorder (SUD). However, the drug intake profile of the SHR in the most relevant animal model of drug addiction, the self-administration (SA) test, and its response on the conditioned place preference (CPP) paradigm are not yet determined. In the present study, we employed SA and CPP tests to investigate the reinforcing effects of the psychostimulant methamphetamine in an SHR substrain obtained from Charles River, Japan (SHR.NCrlCrl). Concurrent tests were also performed in Wistar rats, the strain representing "normal" heterogeneous population.
---
+++ _site/assets/js/custom.js
@@ -0,0 +1,132 @@
+$(document).on('click', 'a', function(event){
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+});
+
+'use strict';
+
+// grabbing the class names from the data attributes
+var navBar = $('.navbar'),
+    data = navBar.data();
+
+var home = document.getElementById("homeNav"),
+    about = document.getElementById("aboutNav"),
+    what = document.getElementById("whatNav"),
+    products = document.getElementById("productsNav"),
+    research = document.getElementById("researchNav"),
+    contact = document.getElementById("contactNav");
+
+
+// booleans used to tame the scroll event listening a little..
+var scrolling = false,
+    scrolledPast = false;
+
+// transition Into
+function switchInto() {
+  // update `scrolledPast` bool
+  scrolledPast = true;
+  // add/remove CSS classes
+  navBar.removeClass(data.startcolor);
+  navBar.addClass(data.intocolor);
+  console.log('into transition triggered!');
+};
+
+// transition Start
+function switchStart() {
+  // update `scrolledPast` bool
+  scrolledPast = false;
+  // add/remove CSS classes
+  navBar.addClass(data.startcolor);
+  navBar.removeClass(data.intocolor);
+  console.log('start transition triggered!');
+}
+
+function pressedHome() {
+  home.style.color = "#FCC72C";
+  about.style.color ="white";
+  what.style.color ="white";
+  products.style.color ="white";
+  research.style.color ="white";
+  contact.style.color ="white";
+};
+
+function pressedAbout() {
+  about.style.color ="#FCC72C";
+  home.style.color = "#848484";
+  what.style.color = "#848484";
+  products.style.color ="#848484";
+  research.style.color ="#848484";
+  contact.style.color ="#848484";
+};
+
+function pressedWhat() {
+  what.style.color ="#FCC72C";
+  about.style.color = "#848484";
+  products.style.color = "#848484";
+};
+
+function pressedProducts() {
+  products.style.color ="#FCC72C";
+  what.style.color = "#848484";
+  research.style.color = "#848484";
+};
+
+function pressedResearch(){
+  research.style.color ="#FCC72C";
+  products.style.color = "#848484";
+  contact.style.color = "#848484";
+};
+
+function pressedContact(){
+  contact.style.color ="#FCC72C";
+  research.style.color = "#848484";
+};
+
+// set `scrolling` to true when user scrolls
+$(window).scroll(function () {
+  return scrolling = true;
+});
+
+setInterval(function () {
+  // when `scrolling` becomes true...
+  if (scrolling) {
+    // set it back to false
+    scrolling = false;
+    // check scroll position
+    if ($(window).scrollTop() > 700) {
+      // user has scrolled > 100px from top since last check
+      if (!scrolledPast) {
+        switchInto();
+      }
+    } else {
+      // user has scrolled back <= 100px from top since last check
+      if (scrolledPast) {
+        switchStart();
+      }
+    };
+
+    if ($(window).scrollTop() < 700) {
+      pressedHome();
+
+    } else if ($(window).scrollTop() < 1800) {
+      pressedAbout();
+
+    } else if ($(window).scrollTop() < 2615) {
+      pressedWhat();
+
+    } else if ($(window).scrollTop() < 3430) {
+      pressedProducts();
+
+    } else if ($(window).scrollTop() < 4610) {
+      pressedResearch();
+
+    } else{
+      pressedContact();
+
+    };
+  };
+  // take a breath.. hold event listener from firing for 100ms
+}, 100);
